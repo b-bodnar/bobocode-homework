a7be2d01a2c4d867dec4aac701e069c95ce5f6d7
Bohdan Bodnar <iloveyoulviv@gmail.com>
2022-03-17 13:34:57 +0200

    Add timecomplexsity

--- algorithm-data-structure/src/main/java/com/bobcode/algorithm/Sorter.java
+++ algorithm-data-structure/src/main/java/com/bobcode/algorithm/Sorter.java
@@ -1,13 +1,34 @@
 package com.bobcode.algorithm;
 
 public interface Sorter {
+
+    /**
+     * timecomplexsity O(n2)
+     *
+     * @param data input data for sorting
+     */
     void bubbleSort(int[] data);
 
+    /**
+     * timecomplexsity O(n2)
+     *
+     * @param data input data for sorting
+     */
     void selectionSort(int[] data);
 
+    /**
+     * timecomplexsity O(n2)
+     *
+     * @param data input data for sorting
+     */
     void insertionSort(int[] data);
 
-    void mergeSort(int[] data, int n);
+    /**
+     * timecomplexsity O(n * log(n))
+     *
+     * @param data input data for sorting
+     */
+    void mergeSort(int[] data);
 
     void margeSortParallel(int[] data);
 }
